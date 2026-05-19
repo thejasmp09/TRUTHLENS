e1dcbb84fd28f14188eb1a7b4c347d9344e4ed8b
Thejas MP <thejasmputtaswamy@gmail.com>
2026-05-19 12:45:43 +0530

    fix: force CORS redeploy

--- api.py
+++ api.py
@@ -24,7 +24,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=False,
+    allow_credentials=False,  #CORS fix
     allow_methods=["*"],
     allow_headers=["*"],
 )
